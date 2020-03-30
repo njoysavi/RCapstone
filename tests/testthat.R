b6e434d27f03d2ca9faea0cf9cbbd3b37a5e063d
njoysavi@gmail.com <njoysavi@users.noreply.github.com>
2020-03-29 18:31:16 -0700

    testthat changes

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,4 @@
 library(testthat)
 library(RCapstone)
-# testthat("Locating data file in package", {
-#   test_data_path <- system.file("extdata", "signif.txt", package="RCapstone")
-#   expect_true( file.exists(test_data_path) )
-# })
+# tests completed are in testthat directory. Please check there
 
